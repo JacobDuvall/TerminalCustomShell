a8b3d08ed3958fab2b525aef3d00395f1a70226b
Jacob Duvall <jdaleduvall@gmail.com>
2019-02-21 17:30:23 +0000

    made changes to outail

--- outail.c
+++ outail.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World4!\n");
+    
+    FILE* file;
+    file = fopen(argv[3], "r");
+    char buffer[1000];
+    int x = 0;
+    int i = 0;
+    int lines = 10; // default number is 10
+    char z;
+
+    for (z = getc(file); z != EOF; z = getc(file)) {
+        x = x+1;
+    }
+    
+    fseek(file, i, SEEK_SET);
+    
+    lines = atoi(argv[2]);
+    
+    while(fgets(buffer, 1000, file))
+    {
+        i = i + 1;
+        if(i > x - lines)
+            fprintf(stdout, "%s", buffer);
+    }
     return 0;
 }
 
